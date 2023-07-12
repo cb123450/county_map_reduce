178d5860aaae1c7e3b2a8d1818c634e464808c48
Timothy Kim <kingoftimothy@gmail.com>
2023-07-12 17:01:09 -0400

    update

--- CovidMapper.java
+++ CovidMapper.java
@@ -1,17 +1,18 @@
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 
 // a county record contains these fields:
 // date,county,state,fips,cases,deaths
 // data is comma delimited
 
-public class CovidMapper extends Mapper<IntWritable, Text, Text, IntWritable> {
+public class CovidMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
   Text outKey = new Text();
   IntWritable outValue = new IntWritable(0);
   
   @Override  
-  public void map(IntWritable key, Text value, Context context) 
+  public void map(LongWritable key, Text value, Context context) 
   throws java.io.IOException, InterruptedException {
     String[] record = value.toString().split(",");
     String state = record[2];
